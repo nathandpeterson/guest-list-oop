7119e893c60a47c638678b846d85e21d5a39d811
Wes Reid <bwreid@gmail.com>
2017-11-22 11:25:11 -0800

    did the thing

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
 console.log(`Let's start a party!`)
+
+// invited vs not, who came vs not, who left vs not
+
+class Venue {
+  constructor (capacity) {
+    this.capacity = capacity
+  }
+}
+
+class Guest {
+  constructor (name, isOver21=false) {
+    this.name = name
+    this.isOver21 = isOver21
+  }
+}
+
+class GuestList {
+  constructor (guests=[]) {
+    this.guests = guests
+  }
+
+  addGuest (guest) {
+    this.guests.push(guest)
+  }
+
+  addGuests(...guests) {
+    this.guests = this.guests.concat(guests)
+  }
+}
+
+class Party {
+  constructor (venue, guestList) {
+    this.currentGuests = []
+    this.pastGuests = []
+
+    this.venue = venue
+    this.guestList = guestList
+  }
+
+  enter (guest) {
+    this.currentGuests.push(guest)
+  }
+
+  leave (guest) {
+    const index = this.currentGuests.indexOf(guest)
+    this.currentGuests.splice(index, 1)
+
+    this.pastGuests.push(guest)
+  }
+
+  get population () {
+    return this.currentGuests.length
+  }
+}
+
+const kevin = new Guest('Kevin', true)
+const nathan = new Guest('Nathan', true)
+const matt = new Guest('Matt', true)
+const sean = new Guest('Sean', true)
+const wes = new Guest('Wes', true)
+
+const venue = new Venue(25)
+const guestList = new GuestList()
+
+guestList.addGuests(kevin, nathan, matt, sean)
+
+const party = new Party(venue, guestList)
+party.enter(matt)
+party.enter(sean)
+party.leave(sean)
+
+console.log(party)
--- readme.md
+++ readme.md
@@ -2,11 +2,11 @@
 
 Let's imagine we're building an IoT project that will track people as the enter and leave a venue. We want to know things like:
 
-- How many people are at the party?
-- Who is inside the party?
-- Who came to the party but left?
-- Who's on the guest list and who is not?
-- Can a new person enter the party? (i.e. What's the max capacity?)
+- [x] How many people are at the party?
+- [x] Who is inside the party?
+- [x] Who came to the party but left?
+- [ ] Who's on the guest list and who is not?
+- [ ] Can a new person enter the party? (i.e. What's the max capacity?)
 
 If you have time, let's add some tracking to see who's the biggest spender!
 
